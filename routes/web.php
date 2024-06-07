c9eb28f4b436435bfb2438bf08703dd4f5a4c7fc
vincentmikhael <vincentmikhael24@gmail.com>
2024-06-08 01:59:05 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -34,16 +34,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', [MainController::class,'index']);
-
-Route::get('/tentang-kami/sejarah-perusahaan',[SejarahController::class,'show']);
-
-Route::get('/carousel',[CarouselController::class,'index']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('/carousel',[CarouselController::class,'index']);
 Route::post('/banner',[CarouselController::class,'add_banner']);
 Route::get('/banner/{id}',[CarouselController::class,'delete_banner']);
-
-Route::post('/youtube',[CarouselController::class,'add_youtube']);
 
 Route::get('/brosur',[BrosurController::class,'index']);
 Route::get('/brosur/add',[BrosurController::class,'add']);
@@ -51,8 +45,6 @@ Route::post('/brosur/add',[BrosurController::class,'add_brosur']);
 Route::get('/brosur/edit/{id}',[BrosurController::class,'edit']);
 Route::post('/brosur/edit/{id}',[BrosurController::class,'update']);
 Route::get('/brosur/delete/{id}',[BrosurController::class,'delete']);
-
-Route::get('/list-brosur',[BrosurController::class,'show']);
 
 Route::get('/cms',[BeritaController::class,'index']);
 Route::get('/cms/add',[BeritaController::class,'add']);
@@ -63,17 +55,12 @@ Route::get('/cms/delete/{id}',[BeritaController::class,'delete']);
 Route::post('/uploadGambarFormBerita',[BeritaController::class,'image_upload'])->name('ckeditor.upload');
 Route::post('/deleteGambarFormBerita',[BeritaController::class,'image_delete'])->name('ckeditor.deleteImage');
 
-Route::get('/berita',[BeritaController::class,'list']);
-Route::get('/berita/{slug}',[BeritaController::class,'show']);
-
 Route::get('/kendaraan',[KendaraanController::class,'index']);
 Route::get('/kendaraan/add',[KendaraanController::class,'add']);
 Route::post('/kendaraan/add',[KendaraanController::class,'add_action']);
 Route::get('/kendaraan/edit/{id}',[KendaraanController::class,'edit']);
 Route::post('/kendaraan/edit/{id}',[KendaraanController::class,'update']);
 Route::get('/kendaraan/delete/{id}',[KendaraanController::class,'delete']);
-
-Route::get('/produk/kendaraan/{slug}',[KendaraanController::class,'show']);
 
 Route::get('/kendaraan/{id_kendaraan}/aksesoris',[KendaraanAksesorisController::class,'index']);
 Route::get('/kendaraan/{id_kendaraan}/aksesoris/add',[KendaraanAksesorisController::class,'add']);
@@ -116,36 +103,28 @@ Route::post('/kendaraan/{id_kendaraan}/lineup/add',[LineupController::class,'add
 Route::get('/kendaraan/{id_kendaraan}/lineup/edit/{id_safety}',[LineupController::class,'edit']);
 Route::post('/kendaraan/{id_kendaraan}/lineup/edit/{id_safety}',[LineupController::class,'update']);
 Route::get('/kendaraan/{id_kendaraan}/lineup/delete/{id_safety}',[LineupController::class,'delete']);
-
+    
 Route::get('/cabang',[CabangController::class,'index']);
 Route::get('/cabang/add',[CabangController::class,'add']);
 Route::post('/cabang/add',[CabangController::class,'add_action']);
 Route::get('/cabang/edit/{id}',[CabangController::class,'edit']);
 Route::post('/cabang/edit/{id}',[CabangController::class,'update']);
 Route::get('/cabang/delete/{id}',[CabangController::class,'delete']);
-Route::get('/dealer',[CabangController::class,'show']);
-Route::get('/api/dealer',[CabangController::class,'api']);
 
 Route::get('/sejarah_perusahaan',[SejarahController::class,'index']);
 Route::post('/sejarah_perusahaan',[SejarahController::class,'add_action']);
 Route::get('/sejarah_perusahaan_timeline/{id}',[SejarahController::class,'delete']);
 Route::get('/bodicat',[BodicatController::class,'index']);
 Route::post('/bodicat',[BodicatController::class,'add_action']);
-Route::get('/aftersales/bodicat',[BodicatController::class,'show']);
 
 Route::get('/aksesoris',[AksesorisController::class,'index']);
 Route::post('/aksesoris',[AksesorisController::class,'add_action']);
-Route::get('/aftersales/aksesoris',[AksesorisController::class,'show']);
 
 Route::get('/cms_sparepart',[CmsSparepartController::class,'index']);
 Route::post('/cms_sparepart',[CmsSparepartController::class,'add_action']);
-Route::get('/aftersales/sparepart',[CmsSparepartController::class,'show']);
-Route::get('/aftersales/sparepart/{id}',[CmsSparepartController::class,'detail']);
-
 
 Route::get('/maintenance',[MaintenanceController::class,'index']);
 Route::post('/maintenance',[MaintenanceController::class,'add_action']);
-Route::get('/aftersales/maintenance',[MaintenanceController::class,'show']);
 
 Route::get('/sparepart_kategori',[SparepartKategoriController::class,'index']);
 Route::get('/sparepart_kategori/add',[SparepartKategoriController::class,'add']);
@@ -161,11 +140,54 @@ Route::get('/sparepart/edit/{id}',[SparepartController::class,'edit']);
 Route::post('/sparepart/edit/{id}',[SparepartController::class,'update']);
 Route::get('/sparepart/delete/{id}',[SparepartController::class,'delete']);
 
+Route::get('/setting',[SettingController::class,'index']);
+Route::post('/setting',[SettingController::class,'add_action']);
+});
+Route::get('/', [MainController::class,'index']);
+
+Route::get('/tentang-kami/sejarah-perusahaan',[SejarahController::class,'show']);
+
+Route::post('/youtube',[CarouselController::class,'add_youtube']);
+
+
+
+Route::get('/list-brosur',[BrosurController::class,'show']);
+
+
+
+Route::get('/berita',[BeritaController::class,'list']);
+Route::get('/berita/{slug}',[BeritaController::class,'show']);
+
+
+
+Route::get('/produk/kendaraan/{slug}',[KendaraanController::class,'show']);
+
+
+Route::get('/dealer',[CabangController::class,'show']);
+Route::get('/api/dealer',[CabangController::class,'api']);
+
+
+Route::get('/aftersales/bodicat',[BodicatController::class,'show']);
+
+Route::get('/aftersales/aksesoris',[AksesorisController::class,'show']);
+
+
+Route::get('/aftersales/sparepart',[CmsSparepartController::class,'show']);
+Route::get('/aftersales/sparepart/{id}',[CmsSparepartController::class,'detail']);
+
+
+
+Route::get('/aftersales/maintenance',[MaintenanceController::class,'show']);
+
+
+
 Route::get('/test-drive',[TestDriveController::class,'show']);
 Route::post('/test-drive',[TestDriveController::class,'add_action']);
 Route::get('/list-testdrive',[TestDriveController::class,'index']);
 
-Route::get('/setting',[SettingController::class,'index']);
-Route::post('/setting',[SettingController::class,'add_action']);
+
 
 Route::get('/kontak',[KontakController::class,'index']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
